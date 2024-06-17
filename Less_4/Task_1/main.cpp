35b45770d822d14df3fe7fed05e0a573ca74028a
“Student” <Student>
2024-06-17 20:40:22 +0300

    Refinement of tests

--- Less_4/Task_1/main.cpp
+++ Less_4/Task_1/main.cpp
@@ -6,14 +6,23 @@
 TEST_CASE("list", "[Task_1]")
 {
     List test;
-    CHECK(test.Empty());
-    test.PushBack(3);
-    test.PushBack(4);
-
-    CHECK(test.Size() == 2);
-    test.Clear();
-
-    CHECK(test.Size() == 0);
+    SECTION("Size") {
+        CHECK(test.Size() == 0);
+        test.PushBack(1);
+        test.PushBack(2);
+        test.PushBack(3);
+        CHECK(test.Size() == 3);
+    }
+    SECTION("Empty") {
+        CHECK(test.Empty() == true);
+        test.PushBack(1);
+        test.PushBack(2);
+        CHECK(test.Empty() == false);
+    }
+    SECTION("Clear") {
+        test.Clear();
+        CHECK(test.Size() == 0);
+    }
 
 }
 
