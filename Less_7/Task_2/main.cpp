65573120c9a644c1c786247d61b9ac50fa0536ae
“Student” <Student>
2024-06-27 19:58:40 +0300

    Add Task_2

---
+++ Less_7/Task_2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+
+int main(int argc, char** argv) {
+
+    int number, chek;
+
+    std::cout << "[IN]: " << std::endl;
+    std::cin >> chek;
+    int count = 0;
+    std::set<int, std::greater<>> mm;
+    while (count < chek) {
+        std::cin >> number;
+        mm.insert(number);
+        count++;
+    }
+    std::cout << "[OUT]: " << std::endl;
+    for (const auto& var : mm) {
+        std::cout << var << std::endl;
+    }
+    return 0;
+}
